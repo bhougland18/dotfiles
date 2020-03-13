519c37b223baf742ed5599046606ba15c5bf3bd9
Benjamin Hougland <benjamin.hougland@gmail.com>
2020-03-13 08:30:31 -0400

    changed shells to use starship instead of powerline go

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,1 @@
-function fish_prompt
-    eval /run/current-system/sw/bin/powerline-go -error $status -shell bare \
-    -newline \
-    -modules "venv,ssh,cwd,perms,git,hg,jobs,exit"
-end
-
-#starship init fish | source
+starship init fish | source
